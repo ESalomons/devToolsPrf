a40670ff4a9cd4f1a8efa21fb599cd4a23a15fbc
Etto Salomons <etto.salomons@gmail.com>
2017-01-20 13:11:29 +0100

    second

---
+++ test/TicketTester.java
@@ -0,0 +1,28 @@
+import helpdesk.data.HardwareTicket;
+import helpdesk.data.Ticket;
+import helpdesk.data.User;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Etto Salomons
+ *         created on 20/01/17.
+ */
+public class TicketTester {
+
+    @Test
+    public void createTicket() {
+        User user = new User("Henk", ",,");
+        Ticket ticket = new HardwareTicket(10, "kapot", user, "abc#$*");
+
+        assertEquals("Henk",ticket.getOwner().getName());
+    }
+
+    @Test
+    public void createTicketFalse() {
+        User user = new User("Henk", ",,");
+        Ticket ticket = new HardwareTicket(10, "kapot", user, "abc#$*");
+
+        assertEquals("Piet",ticket.getOwner().getName());
+    }
+}
